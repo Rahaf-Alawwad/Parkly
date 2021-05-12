75205bb4a46e50c71e084e27bb12766651a1ec44
ahoudkensara <ohoud.kensara@gmail.com>
2021-05-12 10:30:04 +0300

    userLoginSignup

---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as as_view
+from user import views as user_views
+
+urlpatterns = [
+    path('login/' , user_views.login_view ,name="login"),
+    path('logout/' , user_views.logout_view , name= "logout"), 
+    path('signup/' , user_views.signup_view ,name="signup"), 
+    path('profile/' , user_views.profile_view ,name="profile")
+    
+]
