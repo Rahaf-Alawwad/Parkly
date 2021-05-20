4243e64aefecc0783b2770ce24d4fde46d201f18
rahaf-alawwad <rhfalawwad@outlook.com>
2021-05-20 04:47:11 +0300

    deploy

--- Zeta_project/settings.py
+++ Zeta_project/settings.py
@@ -19,7 +19,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -112,6 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 AUTH_USER_MODEL = 'user.User'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Zeta_project/static/'),
 ]
