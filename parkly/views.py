838b333a5c0927670c1ad3470fd6e2d0c86bbd89
w4yqwi <falbdah@gmail.com>
2021-05-20 03:21:54 +0300

    edited Reserve page position and style

--- parkly/views.py
+++ parkly/views.py
@@ -364,7 +364,8 @@ def success(request):
         'distance': distance,
         'minutes':minutes,
         'mapImage':mapImage,
-        'reservation':reservation
+        'reservation':reservation,
+        "lot":lot,
     }
 
     
